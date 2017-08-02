69cebceb09c41021df71bc4f9568e5780892eb83
Barracuda72 <barracuda72@bk.ru>
2017-08-02 22:53:44 +0500

    Fake user-agent

--- imgboards.py
+++ imgboards.py
@@ -2,15 +2,20 @@
 # encoding: utf-8
 
 import json
-import urllib
+from urllib import FancyURLopener
 from HTMLParser import HTMLParser
 import re
 
 html_parser = HTMLParser()
 link_regex = '>>[ ]*([0-9]+)'
 
+class MyURLOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11)  Gecko/20170827 Firefox/52.1.0'
+
+myopener = MyURLOpener()
+
 def get_text(url):
-    f = urllib.urlopen(url)
+    f = myopener.open(url)
     return f.read()
 
 def get_json(url):
